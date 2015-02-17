a51d8a592a42d674ffbe2c3f59331b97d3ea1b0c
Martins Sipenko <martins.sipenko@gmail.com>
2015-02-17 14:49:48 +0200

    added heading, paragraph, image, href

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-Hello World!
+<!doctype html>
+<html>
+  <title>Yo!!!!</title>
+<head>
+<body>
+</body>
+  <h1>Hello World!</h1>
+  <p>My paragraph</p>
+  <form action="" method="post">
+    <label for="name">Name:</label>
+    <input type="text" name="username" id="name">
+    <button type="submit">GO</button>
+  </form>
+  <a href="second.php">Link to other file</a>
+  <img src="http://www.passerellesnumeriques.org/wp-content/uploads/2012/06/accenture-plc-logo.jpg" alt="acenture logo">
+</head>
